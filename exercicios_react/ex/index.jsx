13062e0f189a7e9162da7e86f25eb7be7836eb8a
Paulo Sorrentino <paulo.sorrentino@luizalabs.com>
2017-05-11 13:38:10 -0300

    Aula 23

---
+++ exercicios_react/ex/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './component';
+
+ReactDOM.render(
+    <Component value="I C U"/>, document.getElementById('app')
+);
