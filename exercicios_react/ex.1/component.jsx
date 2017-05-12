232dc1e7694cb6694451d294f4e6befd93d5469b
“Paulo <paulo.sorrentino@luizalabs.com>
2017-05-12 00:14:19 -0300

    Aula 31

---
+++ exercicios_react/ex.1/component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Primeiro = (props) => {
+
+    return <h1>{props.value}</h1>;
+};
+
+export const Segundo = (props) => {
+
+    return <h1>{props.value}</h1>;
+};
+
+// export { Primeiro, Segundo }
--- exercicios_react/ex/component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-    console.log(props);
-    return <h1>{props.value}</h1>;
-}
